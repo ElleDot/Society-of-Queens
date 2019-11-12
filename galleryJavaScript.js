3cc2983826c4da33c25462d3d5ae860e266bf5f1
E~ <elle.dot2019@gmail.com>
2019-11-12 15:17:46 +0000

    Additional Overlays and Cleanup
    Streamlined one part, but created a buggy mess. oops

--- galleryJavaScript.js
+++ galleryJavaScript.js
@@ -1,8 +1,43 @@
-function on() {
-	document.getElementById("overlay").style.display = "block";
-	document.getElementById("overlay").style.opacity = 1;
+function on1() {
+	document.getElementById("overlay1").style.display = "block";
+	document.getElementById("overlay1").style.opacity = 1;
+}
+function on2() {
+	document.getElementById("overlay2").style.display = "block";
+	document.getElementById("overlay2").style.opacity = 1;
+}
+function on3() {
+	document.getElementById("overlay3").style.display = "block";
+	document.getElementById("overlay3").style.opacity = 1;
+}
+function on4() {
+	document.getElementById("overlay4").style.display = "block";
+	document.getElementById("overlay4").style.opacity = 1;
+}
+function on5() {
+	document.getElementById("overlay5").style.display = "block";
+	document.getElementById("overlay5").style.opacity = 1;
+}
+function on6() {
+	document.getElementById("overlay6").style.display = "block";
+	document.getElementById("overlay6").style.opacity = 1;
+}
+function on7() {
+	document.getElementById("overlay7").style.display = "block";
+	document.getElementById("overlay7").style.opacity = 1;
+}
+function on8() {
+	document.getElementById("overlay8").style.display = "block";
+	document.getElementById("overlay8").style.opacity = 1;
 }
 							
 function off() {
-    document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay1").style.display = "none";
+    document.getElementById("overlay2").style.display = "none";
+    document.getElementById("overlay3").style.display = "none";
+    document.getElementById("overlay4").style.display = "none";
+    document.getElementById("overlay5").style.display = "none";
+    document.getElementById("overlay6").style.display = "none";
+    document.getElementById("overlay7").style.display = "none";
+    document.getElementById("overlay8").style.display = "none";
 }
