abcf772c3095117b114f0de892f977af8705316d
E~ <elle.dot2019@gmail.com>
2019-11-18 16:17:40 +0000

    JS Countdown shoertening!
    -Now using one that I don't need to credit!
    
    -Fixed layout of the sidebar on homepage
    
    -Added the event summary on homepage

---
+++ JS/eventsJavaScript.js
@@ -0,0 +1,16 @@
+function openInstagram() {
+	window.open("https://www.instagram.com/thesocietyofqueens/", "_blank");
+}
+
+function openTwitter() {
+	window.open("https://twitter.com/soqinc", "_blank");
+}
+
+function openFacebook() {
+	window.open("https://www.facebook.com/thesocietyofqueens/", "_blank");
+}
+
+function openPaypal() {
+	window.open("https://www.google.co.uk", "_blank");
+}
+
