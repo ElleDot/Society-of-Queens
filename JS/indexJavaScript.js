6a061b55dc0752c66252c40b00539bcd52295602
E~ <elle.dot2019@gmail.com>
2019-11-18 17:25:46 +0000

    Homepage finished??
    -Fixed the calendar's margin and size problems
    
    -Updated some font sizes to be more responsive
    
    -Added some more placeholder text for the homepage

--- JS/indexJavaScript.js
+++ JS/indexJavaScript.js
@@ -3,9 +3,9 @@ window.onscroll = function() {scrollFunction()};
 				
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    	document.getElementById("header").style.fontSize = "30px";
+    	document.getElementById("header").style.fontSize = "3vw";
     } else {
-    	document.getElementById("header").style.fontSize = "50px";
+    	document.getElementById("header").style.fontSize = "5vw";
     }
 }
 	
