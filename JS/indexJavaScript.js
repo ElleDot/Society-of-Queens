847959179fb2698f570045b3a6288ea8f062dd0a
E~ <elle.dot2019@gmail.com>
2019-12-12 16:25:11 +0000

    SEO + Funding page overhaul
    -Added team picture on funding page
    -Added PayPal button on funding page
    -SEO improvements with meta tags and h1 usage

--- JS/indexJavaScript.js
+++ JS/indexJavaScript.js
@@ -5,7 +5,7 @@ function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     	document.getElementById("header").style.fontSize = "3vw";
     } else {
-    	document.getElementById("header").style.fontSize = "5vw";
+    	document.getElementById("header").style.fontSize = "4vw";
     }
 }
 	
