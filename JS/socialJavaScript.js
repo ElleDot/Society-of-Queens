d4959728cae2b74d6597fbe051e1873cbb132adc
E~ <elle.dot2019@gmail.com>
2020-01-11 14:26:17 +0000

    Socials Page :D
    -Added Socials page
    -Added Twitter follow button
    -Added Twitter profile feed
    -Added all spacing

---
+++ JS/socialJavaScript.js
@@ -0,0 +1,19 @@
+function openInstagram() {
+	window.open("https://www.instagram.com/thesocietyofqueens/", "_blank");
+}
+
+function openTwitter() {
+	window.open("https://twitter.com/soqinc", "_blank");
+}
+
+function openFacebook() {
+	window.open("https://www.facebook.com/thesocietyofqueens/", "_blank");
+}
+
+function openPaypal() {
+	var result = confirm("You will be taken to our PayPal donation page."); 
+            if (result == true) { 
+                window.open("https://www.paypal.me/SOQinc", "_blank"); 
+            }
+}
+
